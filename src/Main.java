cbab1f0e481e8ed195e92668f20aa8368e999e92
Dima <akmeev2011@yandex.ru>
2022-08-01 23:10:53 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        byte a = 1;
+        byte a = 2;
         short b = 200;
         int c = 100000000;
         long d = 1000000000L;
